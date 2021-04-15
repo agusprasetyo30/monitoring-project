220e2ed70f220c86ec9cd6f01bdde9aebcb01e93
Apriliaaptr <apriliaaputri14@gmail.com>
2021-04-15 21:20:41 +0700

    update v manager

--- application/views/account/manager/V_manager.php
+++ application/views/account/manager/V_manager.php
@@ -1,84 +1,130 @@
 <!--CONTENT CONTAINER-->
-            <!--===================================================-->
-            <div id="content-container">
-                <div id="page-head"></div>
+                    <!--===================================================-->
+                    <div id="content-container">
+                            
+                            <!--Page Title-->
+                            <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                            <div id="page-title">
+                                <h1 class="page-header text-overflow">Halaman Data Manager </h1>
+                            </div>
+                            <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                            <!--End page title-->
+
+                <!--Breadcrumb-->
+							<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+							<ol class="breadcrumb">
+								<li><a href="<?php echo base_url('dashboard') ?>"><i class="demo-pli-home"></i></a></li>
+								<li><a href="<?php echo base_url('account') ?>">Menu Akun</a></li>
+								<!-- <li><a href="<?php echo base_url('account/viewManager') ?>">Data Manager</a></li> -->
+								<li class="active">Data Manager</li>
+							</ol>
+							<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+							<!--End breadcrumb-->
+                        
 
 
+          
+                
                 <!--Page content-->
                 <!--===================================================-->
                 <div id="page-content">
-                        <div class="panel">
-					        <div class="panel-body">
-					            <div class="fixed-fluid">
-					                <div class="fixed-md-200 pull-sm-left fixed-right-border">
-					
-					                    <!-- Simple profile -->
-					                    <div class="text-center">
-					                        <div class="pad-ver">
-					                            <img src="<?php echo base_url('assets/img/1.png') ?>" class="img-lg img-circle" alt="Profile Picture">
-					                        </div>
-					                        <h4 class="text-lg text-overflow mar-no">Aprilia Putri </h4>
-					                        <p class="text-sm text-muted">Manager of Jargas Prob</p>
-					
-					                        <div class="pad-ver btn-groups">
-					                            <a href="#" class="btn btn-icon demo-pli-facebook icon-lg add-tooltip" data-original-title="Facebook" data-container="body"></a>
-					                            <a href="#" class="btn btn-icon demo-pli-twitter icon-lg add-tooltip" data-original-title="Twitter" data-container="body"></a>
-					                            <a href="#" class="btn btn-icon demo-pli-google-plus icon-lg add-tooltip" data-original-title="Google+" data-container="body"></a>
-					                            <a href="#" class="btn btn-icon demo-pli-instagram icon-lg add-tooltip" data-original-title="Instagram" data-container="body"></a>
-					                        </div>
-					                        <button class="btn btn-block btn-success btn-lg">Follow</button>
-					                    </div>
-					                    <hr>
-					
-					                    <!-- Profile Details -->
-					                    <p class="pad-ver text-main text-sm text-uppercase text-bold">About Me</p>
-					                    <p><i class="demo-pli-map-marker-2 icon-lg icon-fw"></i> Surabaya, Indonesia</p>
-					                    <p><a href="#" class="btn-link"><i class="demo-pli-internet icon-lg icon-fw"></i> http://www.themeon.net</a></p>
-					                    <p><i class="demo-pli-old-telephone icon-lg icon-fw"></i>(123) 456 1234</p>
-					                    <p class="text-sm text-center">Bekerja sejak 2019 di Jargas Probolinggo. </p>
-					
-					
-					                    <hr>
-					                    <p class="pad-ver text-main text-sm text-uppercase text-bold">Skills</p>
-					                    <ul class="list-inline">
-					                        <li class="tag tag-sm">PHP Programming</li>
-					                        <li class="tag tag-sm">Marketing</li>
-					                        <li class="tag tag-sm">Graphic Design</li>
-					                        <li class="tag tag-sm">Sketch</li>
-					                        <li class="tag tag-sm">Photography</li>
-					                    </ul>
-					
-					                    <hr>
-					                    <p class="pad-ver text-main text-sm text-uppercase text-bold">Gallery</p>
-					                    <div class="row img-gallery">
-					                        <div class="col-xs-6">
-					                            <img class="img-responsive" src="<?php echo base_url('assets/img/2.jpg') ?>" alt="thumbs">
-					                        </div>
-					                        <div class="col-xs-6">
-					                            <img class="img-responsive" src="<?php echo base_url('assets/img/2.jpg') ?>" alt="thumbs">
-					                        </div>
-					                      
-					                    </div>
-					                </div>
-					                <div class="fluid">
-					                    <div class="text-right">
-					                        <button class="btn btn-sm btn-primary">Edit Profile</button>
-					                        
-					                    </div>
-					
-					                    <hr class="new-section-md bord-no">
-<!-- 					
-					                    <div class="pad-btm">
-					                        <textarea class="form-control" rows="4" placeholder="What are you thinking?"></textarea>
-					                        <div class="mar-top clearfix">
-					                            <button class="btn btn-sm btn-primary pull-right" type="submit"><i class="demo-psi-right-4 icon-fw"></i> Share</button>
-					                            <a class="btn btn-icon demo-pli-video icon-lg add-tooltip" href="#" data-original-title="Add Video" data-toggle="tooltip"></a>
-					                            <a class="btn btn-icon demo-pli-camera-2 icon-lg add-tooltip" href="#" data-original-title="Add Photo" data-toggle="tooltip"></a>
-					                            <a class="btn btn-icon demo-pli-file icon-lg add-tooltip" href="#" data-original-title="Add File" data-toggle="tooltip"></a>
-					                        </div>
-					                    </div> -->
-					
-					                </div>
-					            </div>
-					        </div>
+				<div class="row pad-btm">
+					    <div class="col-sm-6 toolbar-left">
+                            <a href="<?php echo base_url('account') ?>" id="demo-btn-addrow" class="btn btn-primary btn-labeled"><i class="btn-label ti-arrow-left"></i> Kembali ke menu utama</a>
+                           
 					    </div>
+
+					</div>
+					<div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+					    <div class="eq-height">
+					        <div class="col-sm-4 eq-box-sm">
+					
+					            <!--Panel 1-->
+					            <!--===================================================-->
+					            <div class="panel">
+					                  <div class="panel panel-body" style="border: 1px solid #e0e0e0">
+                                      <p class="text-main text-semibold">Data Manajer Jargas Probolingo</p>
+                                      <h9> Manajer Rayon Pasuruan - Probolinggo</h9>
+									  
+                                      <br><br>
+
+                                   
+                                     
+
+
+                                      <br><br>
+                                      <div class="col-md-12 text-right">
+                                      <!-- <button class="btn btn-sm btn-success btn-labeled"><i class="btn-label ti-import"></i>Unduh Data</button> -->
+					                </div>
+
+					                </div>
+                                   
+					            </div>
+					            <!--===================================================-->
+					            <!--End Panel 1-->
+                               
+					        </div>
+					        <div class="col-sm-8 eq-box-sm">
+					
+					            <!--Panel 2-->
+					            <!--===================================================-->
+					            <div class="panel">
+                                <div class="panel panel-body" style="border: 1px solid #e0e0e0">
+                                <p class="text-main text-semibold">Biodata Manajer</p>
+                                <hr>
+
+								<!-- <a class="media-left" href="#"><img class="img-circle img-sm" alt="Profile Picture" src="assets/img/1.png"></a>
+
+								<p><i class="demo-pli-male icon-lg icon-fw"></i> Nama : </p>
+								<p><i class="demo-pli-map-marker-2 icon-lg icon-fw"></i> Alamat</p>
+					            <p><a href="#" class="btn-link"><i class="demo-pli-internet icon-lg icon-fw"></i> Email :</a></p>
+					            <p><i class="demo-pli-old-telephone icon-lg icon-fw"></i>No Telp :</p> -->
+
+								<div class="comments media-block">
+								<a class="media-left" href="#"><img class="img-circle img-md" alt="Profile Picture" src="../assets/img/profile-photos/4.png"></a>
+					                        <div class="media-body">
+					                            <div class="comment-header">
+					                            <p><i class="demo-pli-male icon-lg icon-fw"></i> Nama : </p>
+													<p><i class="demo-pli-map-marker-2 icon-lg icon-fw"></i> Alamat :</p>
+													<p><a href="#" class="btn-link"><i class="demo-pli-internet icon-lg icon-fw"></i> Email :</a></p>
+													<p><i class="demo-pli-old-telephone icon-lg icon-fw"></i>No Telp :</p>
+					                            </div>
+ 
+					                        </div>
+					                    </div>
+
+								<hr>
+
+								<div class="form-group">
+									<label>Username : </label>				
+								</div>
+
+								<div class="form-group">
+									<label>Password : </label>				
+								</div>
+
+                                 
+                                      <div class="col-md-12 text-right">
+                                      <button class="btn btn-sm btn-success btn-labeled"><i class="btn-label ti-pencil"></i>Sunting</button>
+					                </div>
+
+					            </div>
+					            </div>
+                                </div>
+
+                               
+					            <!--===================================================-->
+					            <!--End Panel 2-->
+					
+                               
+					        </div>
+
+
+					    </div>
+					</div>
+            </div>
+    </div>
+
+         
+    
